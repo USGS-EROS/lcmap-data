f64e225d5e3bcc18611f378cd4f89d4a6cbeec8d
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-03-08 16:03:56 -0600

    Moved dev namespace to where it won't be part of production.

--- project.clj
+++ project.clj
@@ -35,6 +35,7 @@
                      [slamhound "1.5.5"]]
       :plugins [[lein-kibit "0.1.2"]]
       :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+      :source-paths ["dev-resources/src"]
       :env
        {:active-profile "dev"
         ;; Use environment variables for DB configuration:
