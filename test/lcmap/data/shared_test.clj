bce814a77d3952f41f229ea2ff6cc97dad532e7e
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-03 06:56:21 -0500

    Cleanup test configuration.
    1. Use ~/.usgs/lcmap.test.ini as the source of configuration so that when this project is used as a checkout, and files are refreshed using tools.namespace, the shared tests won't produce exceptions. Previously, a project directory relative path was used. This *is* a bit of a work around, but as long as we're consistent, we don't have to come up with some other work-around to conditionally exclude the test directory for all lcmap-* projects that are used using lein checkouts.
    
    2. Renamed test/support/setup_deps.sh to something more general, updated .travis.yml to refer to the proper file.
    
    3. Remove exception handlers from the shared_test.clj -- if exceptions occur during configuration initialization, the error should be obvious. This was done before I added selector metadata to tests and was worried about db / integration tests running on travis-ci.

--- test/lcmap/data/shared_test.clj
+++ test/lcmap/data/shared_test.clj
@@ -3,26 +3,14 @@
             [com.stuartsierra.component :as component]
             [lcmap.data.system :as system]
             [lcmap.data.config :as config]
-            [lcmap.data.components.database :as db]
-            [lcmap.config.helpers :as cfg-help]
-            [leiningen.core.utils :as lein-utils]
-            [leiningen.core.project :as lein-proj]
-            [dire.core :refer [with-handler!]])
-  (:import  [com.datastax.driver.core.exceptions NoHostAvailableException]))
+            [lcmap.config.helpers :as config-helpers]))
 
-(def cfg-opts (merge config/defaults {:ini "test/support/lcmap.test.ini"}))
+(def cfg-file (clojure.java.io/file config-helpers/*lcmap-config-dir* "lcmap.test.ini"))
 
-(with-handler! #'component/start
-  "Build systems that only run unit tests will fail to start the db
-  component"
-  [NoHostAvailableException]
-  (fn [e & args]
-    ;; XXX is there a way to reliably get test selectors?
-    ;;
-    (logging/warn "no db host -- not unusual")
-    args))
+(def cfg-opts (merge config/defaults {:ini cfg-file}))
 
-(def test-system (-> (system/build cfg-opts) (component/start)))
+(def test-system (-> (system/build cfg-opts)
+                     (component/start)))
 
 (def L5 "test/data/ESPA/CONUS/ARD/LT50470272010327-SC20151230101810")
 (def L7 "test/data/ESPA/CONUS/ARD/LC80460272015302-SC20151230102540")
--- test/support/setup_deps.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp test/support/sample_config.ini test/support/lcmap.test.ini
