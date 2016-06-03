91ab0f1ec4b9cda891dc840d9ff635d3e8f6af74
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-03 07:51:33 -0500

    Handle no-db exception, travis cassandra service missing

--- test/lcmap/data/shared_test.clj
+++ test/lcmap/data/shared_test.clj
@@ -1,9 +1,16 @@
 (ns lcmap.data.shared-test
   (:require [clojure.tools.logging :as logging]
             [com.stuartsierra.component :as component]
+            [dire.core :refer [with-handler!]]
             [lcmap.data.system :as system]
             [lcmap.data.config :as config]
             [lcmap.config.helpers :as config-helpers]))
+
+(with-handler! #'component/start
+  [NoHostAvailableException]
+  (fn [e & args]
+    (logging/warn "no db host -- not unusual")
+    args))
 
 (def cfg-file (clojure.java.io/file config-helpers/*lcmap-config-dir* "lcmap.test.ini"))
 
