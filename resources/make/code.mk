6cef0384e70427d59cbe0690103a556742cc8b52
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-14 15:57:53 -0500

    Clean up testing setup/config/make

--- resources/make/code.mk
+++ resources/make/code.mk
@@ -27,20 +27,8 @@ deps-tree:
 loc:
 	@find src -name "*.clj" -exec cat {} \;|wc -l
 
-check: setup-test
-	@lein with-profile
+check:
+	@lein test :all
 
 run:
 	-@lein trampoline run
-
-setup-dev:
-	lein lcmap run-cql --file resources/schema.cql
-
-setup-test:
-	lein lcmap run-cql --file resources/schema.cql
-	lein lcmap make-specs test/data/ESPA/CONUS/ARD/LT50460282002026-SC20160608172634.tar.gz --tile-table conus --tile-keyspace lcmap --tile-size 128:128
-	lein lcmap make-specs test/data/ESPA/CONUS/ARD/LE70460272002002-SC20160608172749.tar.gz --tile-table conus --tile-keyspace lcmap --tile-size 128:128
-	lein lcmap make-tiles test/data/ESPA/CONUS/ARD/*.tar.gz
-
-
-setup: setup-dev setup-test
