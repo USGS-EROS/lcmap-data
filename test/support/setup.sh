4c6c03575a81a6809df2b9a59fadc487ea16d20e
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-16 07:39:57 -0500

    Enable (updated) test setup script

--- test/support/setup.sh
+++ test/support/setup.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 # Configuration
-mkdir ~/.usgs/
+mkdir -p ~/.usgs/
 cp test/support/lcmap.test.ini.example ~/.usgs/lcmap.test.ini
 
-# GDAL
-sudo apt-add-repository ppa:ubuntugis/ppa -y
-sudo apt-get update -qq
-sudo apt-get install libgdal-dev libgdal-java -y
-
 # Cassandra (not available by default on Travis' Trusty build)
-curl --location --silent -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
-sudo apt-get install -y -qq dsc21
+# curl --location --silent -L https://debian.datastax.com/debian/repo_key | sudo apt-key add -
+# sudo apt-get install -y -qq dsc21
