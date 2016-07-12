0dac15a5566745b94f57da400287bbf6d3c4b2cd
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-07-12 14:46:06 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -4,28 +4,28 @@
   :license {:name "NASA Open Source Agreement, Version 1.3"
             :url "http://ti.arc.nasa.gov/opensource/nosa/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.memoize "0.5.8"]
+                 [org.clojure/core.memoize "0.5.9"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.zip "0.1.2"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.cli "0.3.5"]
                  ;; checksum (for debug logging)
-                 [pandect "0.5.4"]
+                 [pandect "0.6.0"]
                  ;; error handling
                  [dire "0.5.4"]
                  ;; data validation and coercion
-                 [prismatic/schema "1.1.0"]
+                 [prismatic/schema "1.1.2"]
                  [camel-snake-kebab "0.4.0"]
                  ;; cassandra / db deps
                  [clojurewerkz/cassaforte "2.0.2"]
                  [net.jpountz.lz4/lz4 "1.3.0"]
-                 [org.xerial.snappy/snappy-java "1.1.2"]
+                 [org.xerial.snappy/snappy-java "1.1.2.6"]
                  ;; file system utils
                  [me.raynes/fs "1.4.6"]
                  [com.stuartsierra/component "0.3.1"]
-                 [leiningen-core "2.5.3"]
+                 [leiningen-core "2.6.1"]
                  [clj-gdal "0.4.0-SNAPSHOT"]
-                 [clj-time/clj-time "0.11.0"]
+                 [clj-time/clj-time "0.12.0"]
                  ;; XXX note that we may still need to explicitly include the
                  ;; Apache Java HTTP client, since the version used by the LCMAP
                  ;; client is more recent than that used by Chas Emerick's
@@ -33,7 +33,7 @@
                  ;; is worked around by explicitly including Apache Java HTTP
                  ;; client library).
                  ;; XXX temp dependencies:
-                 [org.apache.httpcomponents/httpclient "4.5"]
+                 [org.apache.httpcomponents/httpclient "4.5.2"]
                  ;; LCMAP Components
                  [gov.usgs.eros/lcmap-config "0.5.0-SNAPSHOT"]
                  [gov.usgs.eros/lcmap-logger "0.5.0-SNAPSHOT"]]
@@ -53,7 +53,7 @@
              {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
                              [slamhound "1.5.5"]]
               :plugins [[lein-kibit "0.1.2"]
-                        [lein-codox "0.9.4"]]
+                        [lein-codox "0.9.5"]]
               :aliases {"slamhound" ["run" "-m" "slam.hound"]}
               :source-paths ["dev-resources/src"]}
              :test {}})
