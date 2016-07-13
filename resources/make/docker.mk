28772a3adc3e0effdce60067f838d0b5856389af
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-07-13 15:03:47 -0500

    Add docker image build support

--- resources/make/docker.mk
+++ resources/make/docker.mk
@@ -3,19 +3,10 @@ REPO = "lcmap-data"
 COMMIT = $(shell git rev-parse HEAD)
 IMAGE = $(ORG)/$(REPO):$(COMMIT)
 
-#
-# XXX: Beware!
-#
-# Well... the image variable contains the most recent commit.
-# But these still let someone build and push an image using
-# a dirty repository... not sure how much hand holding we need
-# to do?
-#
-
 .PHONY: docker
 
 docker-build:
-	@docker build docker/lcmap-data-docker --tag $(IMAGE)
+	@docker build  -t $(IMAGE) docker/lcmap-data-docker
 
 docker-publish: docker-build
 	@docker push $(IMAGE)
