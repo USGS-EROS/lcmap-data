e886f15dd253f223d403f6ed6428e5927772859b
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-08-31 20:49:32 -0500

    Try to get submodules set up.

--- test/support/travis_install.sh
+++ test/support/travis_install.sh
@@ -14,6 +14,8 @@ sudo apt-add-repository ppa:ubuntugis/ppa -y
 sudo apt-get update -qq
 sudo apt-get install libgdal-dev libgdal-java -y
 echo "In dir: `pwd`"
-sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
-git submodule update --init --recursive
+ls -al
+cd USGS-EROS/lcmap-data && \
+  sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules && \
+  git submodule update --init --recursive
 
